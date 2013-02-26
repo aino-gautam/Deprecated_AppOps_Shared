4b872260c48731a71687148a833e8d1e9ed6b24b
MilindBharambe <milind@ensarm.com>
2013-02-26 12:11:05 +0530

    TypeConstants for Service,User,Space...

---
+++ appops.platform.shared/src/main/java/in/appops/platform/core/typeconstants/TypeConstants.java
@@ -0,0 +1,11 @@
+package in.appops.platform.core.typeconstants;
+
+public class TypeConstants {
+	
+	public static final String USER = "User";
+	
+	public static final String SERVICE = "Service";
+	
+	public static final String SPACE = "Space";
+	
+}
