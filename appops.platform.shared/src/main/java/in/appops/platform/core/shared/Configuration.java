15ed0e6083af2a2d51f048a91d82c8ccdeda0195
nitish <nitish@ensarm.com>
2013-10-04 12:26:34 +0530

    Added method to check and get value of configuration.

--- appops.platform.shared/src/main/java/in/appops/platform/core/shared/Configuration.java
+++ appops.platform.shared/src/main/java/in/appops/platform/core/shared/Configuration.java
@@ -2,15 +2,42 @@ package in.appops.platform.core.shared;
 
 import in.appops.platform.core.entity.Entity;
 
+import java.io.Serializable;
+
 /**
  * The class which contains all configuration details for a widget
  * @author nairutee
  *
  */
+@SuppressWarnings("serial")
 public class Configuration extends Entity {
 	
 	public Configuration() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Checks whether the configuration has the requested property
+	 * @param property
+	 * @return
+	 */
+	public boolean hasConfiguration(String property) {
+		if(getPropertyByName(property) != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the value for the requested property
+	 * @param property
+	 * @return
+	 */
+	public Serializable getConfigurationValue(String property) {
+		if(hasConfiguration(property)) {
+			return getPropertyByName(property);
+		}
+		return null;
 	}
 	
 
