53d8af45593b64b350979e8940fb1fbd4cf4b415
pallavi-ensarm <pallavi@ensarm.com>
2013-11-01 15:41:22 +0530

    configtypes commited

--- entity.configuration.shared/src/main/java/in/appops/platform/client/config/entityconfiguration_type/basic/BooleanConfigType.java
+++ entity.configuration.shared/src/main/java/in/appops/platform/client/config/entityconfiguration_type/basic/BooleanConfigType.java
@@ -1,7 +1,21 @@
 package in.appops.platform.client.config.entityconfiguration_type.basic;
 
+import in.appops.platform.client.config.client_type.BooleanConfig;
 import in.appops.platform.client.config.entityconfiguration_type.ConfigType;
 
 public class BooleanConfigType extends ConfigType {
 	
+	private static final long serialVersionUID = 1L;
+	public static final String	BOOLEAN		= "BooleanConfig";
+	
+	public BooleanConfigType() {
+		setParentConfigtype(BOOLEAN, false, this.getClass(), null, BooleanConfig.class);
+		addProperty(DEFAULT_VALUE, false, false, null, null, null);
+	}
+	
+	@Override
+	public Boolean getDefaultValue() {
+		return (Boolean) super.getDefaultValue();
+	}
+
 }
--- entity.configuration.shared/src/main/java/in/appops/platform/client/config/entityconfiguration_type/basic/StringConfigType.java
+++ entity.configuration.shared/src/main/java/in/appops/platform/client/config/entityconfiguration_type/basic/StringConfigType.java
@@ -3,17 +3,19 @@ package in.appops.platform.client.config.entityconfiguration_type.basic;
 import in.appops.platform.client.config.client_type.StringConfig;
 import in.appops.platform.client.config.entityconfiguration_type.ConfigType;
 
+@SuppressWarnings("serial")
 public class StringConfigType extends ConfigType {
-	public static final String	MAXLENGTH	= "maxlength";
-	public static final String	MINLENGTH	= "minlength";
-	public static final String	VALIDATOR	= "validator";
+	
+	public static final String	MAX_LENGTH	= "maxlength";
+	public static final String	MIN_LENGTH	= "minlength";
+	public static final String CHARSET = "Charset";
 	public static final String	STRING		= "StringConfig";
 	
 	public StringConfigType() {
 		setParentConfigtype(STRING, false, this.getClass(), null, StringConfig.class);
-		addProperty(VALIDATOR, "[A-Z][a-z]", false, null, null, null);
-		addProperty(MAXLENGTH, 500, false, null, null, null);
-		addProperty(MINLENGTH, 0, false, null, null, null);
+		addProperty(REGEX_VALIDATOR, "[A-Z][a-z]", false, null, null, null);
+		addProperty(MAX_LENGTH, 500, false, null, null, null);
+		addProperty(MIN_LENGTH, 0, false, null, null, null);
 	}
 	
 }
