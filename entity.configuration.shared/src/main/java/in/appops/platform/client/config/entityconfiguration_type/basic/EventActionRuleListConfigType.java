ed51c4649046675f41aa664471bbd28bf38f5699
pallavi-ensarm <pallavi@ensarm.com>
2013-11-25 12:28:30 +0530

    configTypes moved to server package

--- entity.configuration.shared/src/main/java/in/appops/platform/client/config/entityconfiguration_type/basic/EventActionRuleListConfigType.java
+++
@@ -1,8 +0,0 @@
-package in.appops.platform.client.config.entityconfiguration_type.basic;
-
-import in.appops.platform.client.config.entityconfiguration_type.ConfigType;
-
-@SuppressWarnings("serial")
-public class EventActionRuleListConfigType extends ConfigType {
-
-}
