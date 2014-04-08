265167589495e746b8dfa610b818e67e76211366
ensarm-komal <komalensarm02>
2014-04-08 11:56:07 +0530

    Added a collections constructor to entitylist
    removed doctype tag from gwt.xml which was giving errors.

--- appops.platform.shared/src/main/java/in/appops/platform/core/util/EntityList.java
+++ appops.platform.shared/src/main/java/in/appops/platform/core/util/EntityList.java
@@ -3,6 +3,7 @@ package in.appops.platform.core.util;
 import in.appops.platform.core.entity.Entity;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class EntityList extends ArrayList<Entity> {
 
@@ -10,47 +11,60 @@ public class EntityList extends ArrayList<Entity> {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private  int startIndex = 0;
-	private  Long maxResult = null;
-	private  int  pageSize = 0;
-	
+	private int startIndex = 0;
+	private Long maxResult = null;
+	private int pageSize = 0;
+
+	public EntityList() {
+	}
+
+	public EntityList(Collection<? extends Entity> c) {
+		super(c);
+	}
+
 	/**
 	 * @return the startIndex
 	 */
 	public int getStartIndex() {
 		return startIndex;
 	}
+
 	/**
-	 * @param startIndex the startIndex to set
+	 * @param startIndex
+	 *            the startIndex to set
 	 */
 	public void setStartIndex(int startIndex) {
 		this.startIndex = startIndex;
 	}
-	
+
 	/**
 	 * @return the maxResult
 	 */
 	public Long getMaxResult() {
 		return maxResult;
 	}
+
 	/**
-	 * @param maxResult the maxResult to set
+	 * @param maxResult
+	 *            the maxResult to set
 	 */
 	public void setMaxResult(Long maxResult) {
 		this.maxResult = maxResult;
 	}
-	
+
 	/**
 	 * @return the pageSize
 	 */
 	public int getPageSize() {
 		return pageSize;
 	}
+
 	/**
-	 * @param pageSize the pageSize to set
+	 * @param pageSize
+	 *            the pageSize to set
 	 */
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
-	
+
 }
